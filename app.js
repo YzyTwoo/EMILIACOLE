f4695b7030955177a0f638e0c7e3d208c3001f00
Tiziano Orieta <tiziorieta@gmail.com>
2023-10-11 18:18:28 -0300

    terminadas todas las paginas

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/music", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/music.html"))
 });
 
+app.get("/about", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/about.html"))
+});
+
 app.use(express.static("public"));
 
 app.listen(3030,()=>{

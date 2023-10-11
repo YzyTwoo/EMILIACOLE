15deaab921b810596048b77ab90fe78a75b74133
Tiziano Orieta <tiziorieta@gmail.com>
2023-10-10 21:19:02 -0300

    Ultimos cambios, agrege rutas nuevas y html nuevos

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.listen(3030,()=>{
-    console.log("Servidor corriendo en el puerto 3030 ");
-});
-
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
 });
@@ -14,4 +10,12 @@ app.get("/contact", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/contact.html"))
 });
 
+app.get("/music", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/music.html"))
+});
+
 app.use(express.static("public"));
+
+app.listen(3030,()=>{
+    console.log("Servidor corriendo en el puerto 3030 ");
+});
